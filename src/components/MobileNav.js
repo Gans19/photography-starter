4aaad6d74b89be7c6a36392d3fde5268d31ee054
SELVA GANESH G <119717606+Gans19@users.noreply.github.com>
2024-08-10 22:11:46 +0530

    initial commit
    initial commit

--- src/components/MobileNav.js
+++ src/components/MobileNav.js
@@ -1,7 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { IoMdClose } from "react-icons/io";
+import { CgMenuRight } from "react-icons/cg";
+import { Link, useLocation } from "react-router-dom";
+import { motion } from "framer-motion";
+
+
+const menuVarients = {
+  hidden: {
+    x: "100%",
+  },
+  show: {
+    x: 0,
+    transition: {
+      ease: [0.6, 0.01, -0.05, 0.9],
+    },
+  },
+};
 
 const MobileNav = () => {
-  return <div>MobileNav</div>;
+  const Location = useLocation()
+  const [openMenu, setOpenMenu] = useState(false);
+
+  useEffect(()=>{
+    setOpenMenu(false)
+  },[Location])
+
+  return (
+    <nav className=" text-primary xl:hidden">
+      <div
+        onClick={() => {
+          console.log("fd");
+          setOpenMenu(true);
+        }}
+        className=" text-3xl  cursor-pointer"
+      >
+        <CgMenuRight />
+      </div>
+      <motion.div
+        variants={menuVarients}
+        initial="hidden"
+        animate={openMenu ? "show" : ""}
+        className=" bg-white shadow-2xl w-full absolute top-0 right-0 max-w-xs h-screen z-20"
+      >
+        <div
+          onClick={() => setOpenMenu(false)}
+          className=" text-4xl absolute z-30 left-4  top-14 text-primary cursor-pointer"
+        >
+          <IoMdClose />
+        </div>
+        <ul className=" h-full ">
+          <li className="  h-full flex flex-col  justify-center text-primary font-primary font-bold text-3xl items-center gap-y-8">
+            <Link to={"/"}>Home</Link>
+            <Link to={"/about"}>About</Link>
+            <Link to={"/portfolio"}>Portfolio</Link>
+            <Link to={"/contact"}>Contact</Link>
+          </li>
+        </ul>
+      </motion.div>
+    </nav>
+  );
 };
 
 export default MobileNav;
--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import {ImFacebook,ImInstagram,ImLinkedin} from "react-icons/im"
 
 const Socials = () => {
-  return <div>Socials</div>;
+  return <div className=' hidden xl:flex ml-24'>
+    <ul className=' flex gap-x-4'>
+      <li><a href='https://wwww.facebook.com' rel="noreferrer" target='_blank'><ImFacebook /></a></li>
+      <li><a href='https://wwww.facebook.com' rel="noreferrer" target='_blank'><ImInstagram /></a></li>
+      <li><a href='https://wwww.facebook.com' rel="noreferrer" target='_blank'><ImLinkedin /></a></li>
+    </ul>
+  </div>;
 };
 
 export default Socials;
